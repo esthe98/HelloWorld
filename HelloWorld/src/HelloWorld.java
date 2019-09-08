65181cdea3248168f29441a3511e1c75247e0445
Jamila Nasreddine <jnasreddine@knights.ucf.edu>
2019-09-07 17:34:03 -0700

    Initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	/* Application that displays
+	 * the following message:
+	 * "Hello,World!"
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello,World!");
+
+	}
+
+}
